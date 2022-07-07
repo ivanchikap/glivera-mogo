4dd5c50348cc9a5050dab8cc19d440c2ecdbb503
Іванчик Антон Петрович <ivanchikap@nsj.gov.ua>
2022-07-07 12:35:07 +0300

    adaptive wedo

---
+++ src/assets/js/accordion.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const accordion = document.getElementById('accordion');
+
+    accordion.addEventListener('click', (e) => {
+        const target = e.target;
+        const accordionItems = accordion.querySelectorAll(".accordion__item");
+        const isAccordionHead = target.classList.contains("accordionHead");
+        const isAccordionHeadSpan = target.classList.contains("accordionHeadSpan");
+
+        if (isAccordionHead || isAccordionHeadSpan) {
+            const accordionItem = target.closest(".accordion__item");
+
+            accordionItems.forEach((item) => {
+                item.classList.remove("active");
+            })
+
+            accordionItem.classList.toggle('active');
+        }
+    })
+});
