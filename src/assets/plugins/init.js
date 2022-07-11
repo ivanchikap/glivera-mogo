a11cefd37be98a3eb643dd0a8b2db9b1447084c3
ivanchikap <anton.ivanchik@gmail.com>
2022-07-11 11:45:10 +0300

    edit carousels

---
+++ src/assets/plugins/init.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#carousel1").owlCarousel(
+        {
+            center: true,
+            items:1,
+            loop:true,
+            nav: true,
+            margin:10,
+            dots: false,
+            responsive:{
+
+            }
+        }
+    );
+    $("#carousel2").owlCarousel(
+        {
+            center: true,
+            items:1,
+            loop:true,
+            nav: true,
+            margin:10,
+            dots: false,
+            responsive:{
+
+            }
+        }
+    );
+
+});
